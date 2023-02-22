80896ba2330ce8f6a77aaa705f88f1adaee5b80b
SOFSV12 <emmanuelsofuwa2@gmail.com>
2023-02-22 09:40:10 -0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./public/*.html"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      kosugi: ["Kosugi Maru"]
+    },
+  },
+  plugins: [],
+}
